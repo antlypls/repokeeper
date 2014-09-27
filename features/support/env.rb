37b450278d985f1143f03430c7803333374799c6
Anatoliy Plastinin <anatoliy.plastinin@gmail.com>
2014-09-27 17:40:57 +0400

    Add rubinius to travis config Increase aruba timeouts to avoid test failures with rbx

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,3 +3,8 @@ require 'fileutils'
 
 bin_dir = File.expand_path(File.dirname(__FILE__) + '/../../bin')
 ENV['PATH'] = "#{bin_dir}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
+
+# increase timeout to avoid timeouts when run with rbx
+Before do
+  @aruba_timeout_seconds = 5
+end
